5b2cd1b3248cfd45dc6b5c87110b133fdc58ae90
BabyBeast1 <lkh01151152@gmail.com>
2023-11-14 12:32:59 +0900

    1

---
+++ src/main/java/com/example/demo/Chapter05JpaReactApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.persistence.Entity;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo", "user.*", "spring.conf"})
+@EntityScan("user.bean")
+@EnableJpaRepositories("user.dao")
+public class Chapter05JpaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter05JpaReactApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/src/user/UploadForm.js
@@ -0,0 +1,162 @@
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
+
+import styles from '../css/UploadForm.module.css';
+import mainImage from '../image/망상토끼.gif';
+import cameraImg from '../image/camera.png';
+import axios from 'axios';
+
+const UploadForm = () => {
+    const imgRef = useRef()
+
+    const [userUploadDTO, setUserUploadDTO] = useState({
+        imageName: '',
+        imageContent: '',
+        imageFileName: '',
+        imageOriginalName: ''
+    })
+    const { imageName, imageContent, imageFileName, imageOriginalName } = userUploadDTO
+
+    const [imgList, setImgList] = useState([])
+    const [file, setFile] = useState('')
+    const [files, setFiles] = useState('')
+
+    const navigate = useNavigate()
+
+    const onInput = (e) => {
+        const { name, value } = e.target
+
+        setUserUploadDTO({
+            ...userUploadDTO,
+            [name]: value
+        })
+    }
+
+    const onCamera = () => {
+        imgRef.current.click()
+    }
+    
+    const onImgInput = (e) => {
+        const imgFiles = Array.from(e.target.files)
+        var imgArray = []
+
+        imgFiles.map(item => {
+            const objectURL = URL.createObjectURL(item)
+            imgArray.push(objectURL)
+        })
+
+        setImgList(imgArray) //카메라 아이콘을 누르면 이미지 미리보기 용도
+        setFiles(e.target.files) //formData에 넣어서 서버로(스프링 부트) 보내기
+    }
+
+    const onUploadSubmit = (e) => {
+        e.preventDefault()
+
+        var formData = new FormData()
+
+        formData.append( 'userUploadDTO', new Blob([JSON.stringify(userUploadDTO)], {type: 'application/json' }) )
+        // for(var i=0; i<files.length; i++){
+        //     formData.append('img', files[i])
+        // }    // 아래 오브젝트와 같은 성능을 가짐
+        Object.values(files).map((item, index) => {
+            formData.append('img', item)
+        })
+
+        axios.post('/user/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(res => {
+            alert('이미지 업로드 완료')
+            navigate('/user/uploadList')
+        })
+        .catch(error => console.log(error))
+    }
+
+    const onReset = (e) => {
+        e.preventDefault()
+
+        setUserUploadDTO({
+            imageName: '',
+            imageContent: '',
+            imageFileName: '',
+            imageOriginalName: ''
+        })
+
+        setImgList([])
+        imgRef.current.value = ''
+    }
+
+    // const readURL = (input) => {
+    //     var reader = new FileReader();
+    //     reader.readAsDataURL(input.files[0])
+
+    //     reader.onload = () => {
+    //         console.log(input.files[0])
+    //         setShowImgSrc(reader.result)
+    //         setFile(input.files[0])
+
+    //     }
+    // }
+
+    return (
+        <div>
+            <h3>
+                <Link to='/'>
+                <img src={ mainImage } alt='망상토끼' width='100' height='100' />
+                </Link>
+            </h3>
+            <form className={ styles.UploadForm}>
+                <table border={1}>
+                    <thead/>
+                    <tbody>
+                        <tr>
+                            <th>상품명</th>
+                            <td>
+                                <input type="text" name="imageName" size='35' value={ imageName } onChange={ onInput } />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan={2} align="center">
+                                <textarea name='imageContent' rows='10' cols='60' value={ imageContent } onChange={ onInput } ></textarea>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan={2}>
+                                <span>
+                                    {
+                                        // 선택한 이미지를 미리보기
+                                        imgList.map((item, index) => <img key={ index } 
+                                                                          src={ item } 
+                                                                          style={{ width: '100px', height: '100px' }} />)
+                                    }
+                                </span>
+
+                                <img id="camera" src={ cameraImg } alt="카메라"
+                                     onClick={ onCamera }
+                                     style={{ width: 40, height: 40, borderRadius: 20 }} />
+                                <input type="file" name="img[]" multiple='multiple'
+                                    ref={ imgRef }
+                                    onChange={ onImgInput }
+                                    style={{visibility: "hidden"}} />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan={2} align="center">
+                                <button onClick={ onUploadSubmit }>이미지 업로드</button> &nbsp;
+                                <button onClick={ onReset }>취소</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot/>
+                </table>
+            </form>
+        </div>
+    );
+};
+
+export default UploadForm;
